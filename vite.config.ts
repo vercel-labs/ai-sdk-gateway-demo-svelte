6c4fafd97752974b5908b22d538f68e0cd5236d8
Walter Korman <shaper@vercel.com>
2025-04-05 22:27:32 -0700

    feat: redo with sv@0.6.18 (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import tailwindcss from "@tailwindcss/vite";
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+    plugins: [sveltekit()],
+
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    }
 });
