b297786d8e66d1c0830f733d69190002c0c7f26a
Walter Korman <shaper@vercel.com>
2025-06-19 10:43:19 -0700

    fix typecheck

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -1,17 +1,18 @@
-import Root, {
-	type ButtonProps,
-	type ButtonSize,
-	type ButtonVariant,
-	buttonVariants,
-} from "./button.svelte";
+import Root from "./button.svelte";
+import {
+  type ButtonProps,
+  type ButtonSize,
+  type ButtonVariant,
+  buttonVariants,
+} from "./button-variants.js";
 
 export {
-	Root,
-	type ButtonProps as Props,
-	//
-	Root as Button,
-	buttonVariants,
-	type ButtonProps,
-	type ButtonSize,
-	type ButtonVariant,
+  Root,
+  type ButtonProps as Props,
+  //
+  Root as Button,
+  buttonVariants,
+  type ButtonProps,
+  type ButtonSize,
+  type ButtonVariant,
 };
